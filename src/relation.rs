6aec31a807cc116a0140e773a86129eff9097f93
Arnaud Sok <arnaudsokprivate@gmail.com>
2024-11-19 00:18:46 +0100

    Finalisation du TP5

--- src/relation.rs
+++ src/relation.rs
@@ -1,19 +1,11 @@
 use bytebuffer::ByteBuffer;
-
-use string_builder::Builder;
-use crate::buffer::{self, Buffer};
+use crate::buffer::Buffer;
 use crate::col_info::ColInfo;
-use crate::page::{self, PageId};
+use crate::page:: PageId;
 use crate::record::Record;
-use std::borrow::Borrow;
-use std::fs::OpenOptions;
-use std::io::{self, Write};
-use std::cell::{Ref, RefCell};
+use std::cell:: RefCell;
 use crate::buffer_manager::BufferManager;
 use crate::record_id::RecordId;
-use crate::DBConfig;
-use crate::disk_manager::DiskManager;
-use std::env;
 use std::rc::Rc;
 
 pub struct Relation<'a> { //PERSONNE(NOM,PRENOM?,AGE)
@@ -33,34 +25,11 @@ impl<'a> Relation<'a> {
 
         //On appelle 'alloc_page' avant de déplacer 'buffer_manager' sinon ça fais des chinoiseries
         let header_page_id = bfm.get_disk_manager_mut().alloc_page();
-        //println!("file idx {}",header_page_id.get_FileIdx());
-        //println!("page idx {}",header_page_id.get_PageIdx());
-
-        //let mut x = bfm.get_page(&header_page_id);
-        //println!("{:?}",x.get_mut_buffer().as_bytes());
+    
         bfm.get_page(&header_page_id).write_int(0, 0);
-        //println!("{:?}",x.get_mut_buffer().as_bytes());
-        bfm.free_page(&header_page_id, true);
-        //println!("first dirty constru {:?}",bfm.get_liste_pages()[0].get_dirty());
-        //println!("Page id du construc");
-        //println!("{}",bfm.get_liste_pages()[0].get_page_id().get_FileIdx());
-        //println!("{}",bfm.get_liste_pages()[0].get_page_id().get_PageIdx());
-
-        /* 
-        println!("contenu buffer1 : {:?}",bfm.get_liste_buffer()[0].borrow());
-        println!("contenu buffer2 : {:?}",bfm.get_liste_buffer()[1].borrow());
-        println!("contenu buffer3 : {:?}",bfm.get_liste_buffer()[2].borrow());
-        println!("contenu buffer4 : {:?}",bfm.get_liste_buffer()[3].borrow());
        
-        
-        //println!("second dirty constru {}",bfm.get_liste_pages()[0].get_dirty());
-        println!("Taille vec page info : {}",bfm.get_liste_pages().len());
-        println!("Taille vec buff : {}",bfm.get_liste_buffer().len());
-        println!("contenu buffer1 : {:?}",bfm.get_liste_buffer()[0].borrow());
-        println!("contenu buffer2 : {:?}",bfm.get_liste_buffer()[1].borrow());
-        println!("contenu buffer3 : {:?}",bfm.get_liste_buffer()[2].borrow());
-        println!("contenu buffer4 : {:?}",bfm.get_liste_buffer()[3].borrow());*/
-
+        bfm.free_page(&header_page_id, true);
+      
         bfm.flush_buffers();
        
         Relation {
@@ -450,113 +419,42 @@ impl<'a> Relation<'a> {
 
 
     pub fn addDataPage(&mut self) -> () {
-    // Emprunt mutable de buffer_manager pour effectuer toutes les opérations
-    let mut buffer_manager = self.buffer_manager.borrow_mut();
-    let nb_octets_restant = buffer_manager.get_disk_manager().get_dbconfig().get_page_size() as u32;
+        // Emprunt mutable de buffer_manager pour effectuer toutes les opérations
+        let mut buffer_manager = self.buffer_manager.borrow_mut();
+        let mut nb_octets_restant = buffer_manager.get_disk_manager().get_dbconfig().get_page_size() as u32 ;
     
 
-    //let x = buffer_manager.get_liste_pages()[0].get_dirty();
+        // Allocation de la nouvelle page
+        let nouvelle_page = buffer_manager.get_disk_manager_mut().alloc_page();
 
-    // Allocation de la nouvelle page
-    let nouvelle_page = buffer_manager.get_disk_manager_mut().alloc_page();
-
-    // Accès et manipulation de la page d'en-tête
-    let mut header_page = buffer_manager.get_page(&self.header_page_id); // Emprunt mutable de la page d'en-tête
-
-    //println!("{}",x);
-  
-
-    let mut nb_pages = header_page.read_int(0).unwrap();
-    nb_pages += 1; // Incrémentation du nombre de pages
-    header_page.write_int(0, nb_pages);
-
-    let next_offset = 4 + (nb_pages - 1) * 12; // Calcul de l'offset pour l'écriture des données
-
-    // Écriture des informations sur la nouvelle page
-    header_page.write_int(next_offset as usize, nouvelle_page.get_FileIdx() as i32);
-    header_page.write_int((next_offset + 4) as usize, nouvelle_page.get_PageIdx() as i32);
-
-    // Calcul de la taille restante de la page
-   
-    header_page.write_int((next_offset + 8) as usize, nb_octets_restant as i32);
-    //println!("{:?}",header_page.get_mut_buffer().as_bytes());
-
-    //drop(header_page);
-
-    /* 
-    let x = buffer_manager.get_liste_pages()[0].get_dirty();
-    //let x2 = buffer_manager.get_liste_pages()[1].get_dirty();
-    let x3 = buffer_manager.get_liste_pages()[0].get_pin_count();
-    //let x4 = buffer_manager.get_liste_pages()[1].get_pin_count();
-    let x5 = buffer_manager.get_liste_pages()[0].get_page_id().get_FileIdx();
-    let x6 = buffer_manager.get_liste_pages()[0].get_page_id().get_PageIdx();
-    //let x7 = buffer_manager.get_liste_pages()[1].get_page_id().get_FileIdx();
-    //let x8 = buffer_manager.get_liste_pages()[1].get_page_id().get_PageIdx();
-
-    
-    println!("{}",x);
-    //println!("{}",x2);
-    println!("{}",x3);
-    //println!("{}",x4);
-    println!("Page idx  dans buffer 1: {},{}",x5,x6);
-    //println!("Page idx  dans buffer 2: {},{}",x7,x8);
-
-    */
-    // Libération des pages en fin de traitement
-
-
-    //let x15 = buffer_manager.get_liste_pages()[0].get_dirty();
-    //println!("first dirty {}",x15);
-
-    buffer_manager.free_page(&self.header_page_id, true); // Libération de la page d'en-tête
-
-  
-    let mut dataPage = buffer_manager.get_page(&nouvelle_page);
-    dataPage.write_int((nb_octets_restant-4) as usize, 0);
-    dataPage.write_int((nb_octets_restant-8) as usize, 0);
-    buffer_manager.free_page(&nouvelle_page, true);
-
-    //let x13 = buffer_manager.get_liste_pages()[0].get_dirty();
-    //println!("second dirty {}",x13);
-
-    //println!("contenu buffer1 : {:?}",buffer_manager.get_liste_buffer()[0].borrow());
-    //println!("contenu buffer2 : {:?}",buffer_manager.get_liste_buffer()[1].borrow());
-    //println!("contenu buffer3 : {:?}",buffer_manager.get_liste_buffer()[2].borrow());
-    //println!("contenu buffer4 : {:?}",buffer_manager.get_liste_buffer()[3].borrow());
-
-    buffer_manager.flush_buffers();
-
-    //println!("Taille vec page info : {}",buffer_manager.get_liste_pages().len());
-    //println!("Taille vec buff : {}",buffer_manager.get_liste_buffer().len());
-    //println!("contenu buffer1 : {:?}",buffer_manager.get_liste_buffer()[0].borrow());
-    //println!("contenu buffer2 : {:?}",buffer_manager.get_liste_buffer()[1].borrow());
-    //println!("contenu buffer3 : {:?}",buffer_manager.get_liste_buffer()[2].borrow());
-    //println!("contenu buffer4 : {:?}",buffer_manager.get_liste_buffer()[3].borrow());
-    //let x5 = buffer_manager.get_liste_pages()[0].get_page_id().get_FileIdx();
-    //let x6 = buffer_manager.get_liste_pages()[0].get_page_id().get_PageIdx();
-    //let x10 = buffer_manager.get_liste_pages()[0].get_dirty();
-    //println!("Page idx  dans le premier buffer : {},{}",x5,x6);
-   //println!("second dirty {}",x10);
-    //buffer_manager.free_page(&nouvelle_page, false);     // Libération de la nouvelle page
-    // Finalisation de l'écriture des buffers
-        /* 
-       
-       
+        // Accès et manipulation de la page d'en-tête
+        let mut header_page = buffer_manager.get_page(&self.header_page_id); // Emprunt mutable de la page d'en-tête
+        let mut nb_pages = header_page.read_int(0).unwrap();
+        nb_pages += 1; // Incrémentation du nombre de pages
+        header_page.write_int(0, nb_pages);
         
-        //println!("second dirty constru {}",bfm.get_liste_pages()[0].get_dirty());
-        println!("Taille vec page info : {}",bfm.get_liste_pages().len());
-        println!("Taille vec buff : {}",bfm.get_liste_buffer().len());
-        println!("contenu buffer1 : {:?}",bfm.get_liste_buffer()[0].borrow());
-        println!("contenu buffer2 : {:?}",bfm.get_liste_buffer()[1].borrow());
-        println!("contenu buffer3 : {:?}",bfm.get_liste_buffer()[2].borrow());
-        println!("contenu buffer4 : {:?}",bfm.get_liste_buffer()[3].borrow());*/
+
+        let next_offset = 4 + (nb_pages - 1) * 12; // Calcul de l'offset pour l'écriture des données
+
+        // Écriture des informations sur la nouvelle page
+        header_page.write_int(next_offset as usize, nouvelle_page.get_FileIdx() as i32);
+        header_page.write_int((next_offset + 4) as usize, nouvelle_page.get_PageIdx() as i32);
+
+        // Calcul de la taille restante de la page
+        header_page.write_int((next_offset + 8) as usize, (nb_octets_restant - 8 ) as i32);
     
-   
+        buffer_manager.free_page(&self.header_page_id, true); // Libération de la page d'en-tête
+
+        //Écriture des trucs à la fin dans la datapage.
+        let mut dataPage = buffer_manager.get_page(&nouvelle_page);
+        dataPage.write_int((nb_octets_restant-4) as usize, 0);
+        dataPage.write_int((nb_octets_restant-8) as usize, 0);
+        buffer_manager.free_page(&nouvelle_page, true);
+
+        
+        buffer_manager.flush_buffers();
+
 }
-
-    
-
-
 
 
     //Je retourne un Option car je veux que si je trouve rien, je retourne genre "null"
@@ -570,9 +468,12 @@ impl<'a> Relation<'a> {
 
             let offset = 4 + i * 12;
 
-            if (size_record <=  buffer_manager.get_page(&self.header_page_id).read_int((offset + 8) as usize).unwrap() as usize) {
+           
+            
+            if (size_record + 8  <=  buffer_manager.get_page(&self.header_page_id).read_int((offset + 8) as usize).unwrap() as usize )  {
 
                 let page = Some(PageId::new(buffer_manager.get_page(&self.header_page_id).read_int(offset as usize).unwrap() as u32, buffer_manager.get_page(&self.header_page_id).read_int((offset + 4) as usize).unwrap() as u32));
+              
 
                 buffer_manager.free_page(&self.header_page_id, false);
                 buffer_manager.free_page(&self.header_page_id, false);
@@ -598,7 +499,9 @@ impl<'a> Relation<'a> {
 
         // Lecture des données nécessaires une seule fois
         let position_libre = page.read_int((page_size - 4) as usize).unwrap() as usize;
+
         let taille_record: usize = self.write_record_to_buffer(record, &mut page, position_libre);
+        println!("TAILLE RECORD {}",taille_record);
 
         let m_nb_slot: usize = page.read_int((page_size - 8) as usize).unwrap() as usize;
 
@@ -608,6 +511,7 @@ impl<'a> Relation<'a> {
 
         let taille_pos: usize = m_nb_slot * 8; // Taille totale des couples (position, taille) déjà présents
 
+        
         // Écriture du couple (position, taille) pour le record actuel
         page.write_int((page_size as usize) - 8 - taille_pos - 8, position_libre as i32);
         page.write_int((page_size as usize) - 8 - taille_pos - 4, taille_record as i32);
@@ -618,12 +522,13 @@ impl<'a> Relation<'a> {
         let mut header_page = buffer_manager.get_page(&self.header_page_id);
         for i in 0..header_page.read_int(0).unwrap() {
             let offset = 4 + i * 12;
-
+            
             if header_page.read_int(offset as usize).unwrap() == (page_id.get_FileIdx() as i32)
                 && header_page.read_int((offset + 4) as usize).unwrap() == (page_id.get_PageIdx() as i32)
             {
                 let tmp = header_page.read_int((offset + 8) as usize).unwrap();
-                header_page.write_int((offset + 8) as usize, tmp - taille_totale as i32);
+                header_page.write_int((offset + 8) as usize, tmp - taille_totale as i32)  ;
+                break;
             }
         }
 
@@ -656,14 +561,9 @@ impl<'a> Relation<'a> {
 
             let mut record = Record::new(vec);
 
-            //println!("Lecture du record {} : pos avant lecture = {}", i, pos);
-        
          
             pos = pos + self.read_from_buffer(&mut record, &buffer_data, pos);
 
-
-            //println!("pos après lecture : {}", pos);
-            //println!("record après lecture : {:?}", record);
 
             listeDeRecords.push(record);
     
@@ -709,6 +609,7 @@ impl<'a> Relation<'a> {
         //on récupère une page avec assez de place pour écrire
         let data_page = self.get_free_data_page_id(taille_record);
         
+        //Incroyable, Optimisation niveau Master, si question demander à Aymeric
         if(data_page.is_none()){
             self.addDataPage();
             let data_page = (self.get_free_data_page_id(taille_record)).unwrap();
@@ -727,7 +628,9 @@ impl<'a> Relation<'a> {
         
         for page in liste_data_pages.iter() {
             let mut liste_record_tmp = self.get_records_in_data_page(page);
+            print!("PAGE : {:?}",page);
             liste_records.append(&mut liste_record_tmp);
+            println!("Liste record tmp : {:?}",liste_record_tmp);
         }
         
         return liste_records;
@@ -740,8 +643,10 @@ impl<'a> Relation<'a> {
 #[cfg(test)]
 mod tests{
 
-    
-    use std::borrow::Borrow;
+    use crate::DBConfig;
+    use crate::disk_manager::DiskManager;
+
+   
     use super::*;
     use std::rc::Rc;
     
@@ -755,7 +660,6 @@ mod tests{
         let algo_lru = String::from("LRU");
         
         let mut buffer_manager = BufferManager::new(&config, dm, &algo_lru);
-
 
 
 
@@ -836,7 +740,6 @@ mod tests{
 
     fn test_addDataPage() {
 
-        env::set_var("RUST_BACKTRACE", "1");
 
         let chemin = String::from("res/dbpath/BinData");
         let s: String = String::from("res/fichier.json");
@@ -854,6 +757,8 @@ mod tests{
         let mut relation = Relation::new("PERSONNE".to_string(),colinfo.clone(),buffer_manager);
         relation.addDataPage();
         relation.addDataPage();
+        relation.addDataPage();
+        relation.addDataPage();
         
 
     }    
@@ -861,8 +766,6 @@ mod tests{
 
     #[test]
     fn test_get_free_dataPage () {
-
-        env::set_var("RUST_BACKTRACE", "1");
 
         let chemin = String::from("res/dbpath/BinData");
         let s: String = String::from("res/fichier.json");
@@ -1043,9 +946,10 @@ mod tests{
         ];
         let mut relation = Relation::new("PERSONNE".to_string(),colinfo.clone(),buffer_manager);
 
+
         let record1 = Record::new(vec!["SOK".to_string(),"ARNAUD".to_string(),"20".to_string()]);
         let record2 = Record::new(vec!["MEUNIER".to_string(),"YOHANN".to_string(),"20".to_string()]);
-        let record3 = Record::new(vec!["Moustache".to_string(),"MATHIEU".to_string(),"20".to_string()]);
+        let record3 = Record::new(vec!["MOUST".to_string(),"MATH".to_string(),"20".to_string()]);
         let record4 = Record::new(vec!["LETACONNOUX".to_string(),"AYMERIC".to_string(),"20".to_string()]);
         let record5 = Record::new(vec!["CHIBANNI".to_string(),"RAMZY".to_string(),"20".to_string()]);
         let record6 = Record::new(vec!["BOTKZ".to_string(),"LEFOU".to_string(),"89".to_string()]);
@@ -1058,7 +962,7 @@ mod tests{
         let rid4 = relation.insert_record(record4);
         let rid5 = relation.insert_record(record5);
         let rid6 = relation.insert_record(record6);
-        let rid7 = relation.insert_record(record7);
+        let rid7 = relation.insert_record(record7); 
 
 
         let listRecord = relation.get_all_records();
