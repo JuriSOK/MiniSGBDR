e0cd02fa0eeb371f935206ae77eb497ff240681f
Shrek1515 <landepadel@outlook.fr>
2024-11-28 19:27:33 +0100

    correction de pb dans le constructeur, jsp si ça fonctionne

--- src/sgbd.rs
+++ src/sgbd.rs
@@ -16,24 +16,32 @@ use crate::buffer_manager;
 use crate::db_manager::DBManager; 
 use crate::disk_manager::DiskManager; 
 pub struct Sgbd<'a> {
-    dbconfig : DBConfig,
-    buffer_manager : BufferManager<'a>,
+    dbconfig : &'a DBConfig,
+    buffer_manager : Rc<RefCell<BufferManager<'a>>>,
     db_manager : DBManager<'a>,
 
 }
 
 impl <'a>Sgbd<'a> {
     pub fn new(db : DBConfig) -> Self {
-        
-        
+
+        /*
         let mut tmp = DiskManager::new(&db);
         tmp.load_state();
-         
+
         let tmp_buffer_m = BufferManager::new(&db,tmp,"LRU".to_string());
         Sgbd {
-            dbconfig: db,
+            dbconfig: &db,
             buffer_manager : tmp_buffer_m,
             db_manager : DBManager::new(&db, Rc::new(RefCell::new(tmp_buffer_m))),
+        }
+        */
+
+        let rc_bfm = Rc::new(RefCell::new(BufferManager::new(&db, DiskManager::new(&db), "LRU".to_string())));
+        Sgbd{
+            dbconfig: &db,
+            buffer_manager: Rc::clone(&rc_bfm),
+            db_manager: DBManager::new(&db, rc_bfm), //DBManager::new(&db, Rc::clone(&rc_bfm))
         }
     }
 
